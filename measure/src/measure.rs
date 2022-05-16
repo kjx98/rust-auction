627ed86a5e449b41c2a74de18e71805154d47059
Jesse Kuang <jkuang@21cn.com>
2022-05-16 17:18:46 +0800

    add x86_64 rdtscp support

--- measure/src/measure.rs
+++ measure/src/measure.rs
@@ -2,6 +2,7 @@ use {
     std::{fmt, time::{Duration, Instant}},
 };
 
+
 #[derive(Debug)]
 pub struct Measure {
     name: &'static str,
@@ -13,6 +14,96 @@ fn duration_as_ns(d: &Duration) -> u64 {
     d.as_secs()
         .saturating_mul(1_000_000_000)
         .saturating_add(u64::from(d.subsec_nanos()))
+}
+
+#[cfg(target_arch = "x86_64")]
+#[allow(dead_code)]
+mod x86 {
+    use std::sync::Once;
+    static INIT: Once = Once::new();
+    static mut DUR_TEST: u64 = 0;
+    static mut TICKS_TEST: u64 = 1;
+    static mut TICKS_OVERHEAD: u64 = 1_000_000;
+    use std::time::{Instant, Duration};
+    use super::duration_as_ns;
+
+    pub fn check_rdtscp() -> bool {
+        use std::arch;
+        let result = unsafe {
+                        arch::x86_64::__cpuid_count(0x80000001u32, 0) };
+        (result.edx & (1 << 27)) != 0
+    }
+
+    fn rdtscp() -> u64 {
+        use std::arch::asm;
+        let edx: u32;
+        let eax: u32;
+        unsafe {
+            asm!("rdtscp",
+                 out("edx") edx,
+                 out("eax") eax,
+                 out("cx") _);
+        }
+        ((edx as u64) << 32) + (eax as u64)
+    }
+
+    fn calibration() {
+        if ! check_rdtscp() { return }
+        // calibrication overhead
+        for _i in 0 .. 100 {
+            let start = rdtscp();
+            let stop = rdtscp();
+            let diff = stop - start;
+            unsafe {
+                if TICKS_OVERHEAD > diff {
+                    TICKS_OVERHEAD = diff
+                }
+            }
+        }
+        let t_start = Instant::now();
+        let start = rdtscp();
+        std::thread::sleep(Duration::from_millis(100));
+        let stop = rdtscp();
+        unsafe {
+            DUR_TEST = duration_as_ns(& t_start.elapsed());
+            TICKS_TEST = stop - start;
+        }
+    }
+
+    pub struct MeasureTsc {
+        name: &'static str,
+        start: u64,
+        duration: u64,
+    }
+    impl MeasureTsc {
+        pub fn start(name: &'static str) -> Self {
+            INIT.call_once(|| {
+                calibration();
+            });
+            assert!(check_rdtscp(), "rdtscp NOT SUPPORTED!");
+            Self {
+                name,
+                start: rdtscp(),
+                duration: 0,
+            }
+        }
+        pub fn stop(&mut self) {
+            self.duration = unsafe {
+                (rdtscp() - self.start - TICKS_OVERHEAD)
+                    .saturating_mul(DUR_TEST)
+                    / TICKS_TEST
+            }
+        }
+        pub fn as_ns(&self) -> u64 {
+            self.duration
+        }
+        pub fn as_us(&self) -> u64 {
+            self.duration / 1000
+        }
+        pub fn as_ms(&self) -> u64 {
+            self.duration / (1000 * 1000)
+        }
+    }
 }
 
 impl Measure {
@@ -120,6 +211,21 @@ mod tests {
         sleep(Duration::from_secs(1));
         measure.stop();
         assert!(measure.as_s() >= 0.99f64 && measure.as_s() <= 1.01f64);
+        assert!(measure.as_ms() >= 990 && measure.as_ms() <= 1_010);
+        assert!(measure.as_us() >= 999_000 && measure.as_us() <= 1_010_000);
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn test_measure_tsc() {
+        use super::x86::MeasureTsc;
+        if ! super::x86::check_rdtscp() {
+            println!("NO rdtscp SUPPORT");
+            return
+        }
+        let mut measure = MeasureTsc::start("test");
+        sleep(Duration::from_secs(1));
+        measure.stop();
         assert!(measure.as_ms() >= 990 && measure.as_ms() <= 1_010);
         assert!(measure.as_us() >= 999_000 && measure.as_us() <= 1_010_000);
     }
